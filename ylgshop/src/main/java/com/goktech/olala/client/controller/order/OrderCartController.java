6e089f7084f41e8528932efdfb8ebc1bc1b77e0b
sanming <841657622@qq.com>
2020-11-28 10:26:19 +0800

    controller包下新增order包，创建一个订单购物车的controller类

---
+++ ylgshop/src/main/java/com/goktech/olala/client/controller/order/OrderCartController.java
@@ -0,0 +1,14 @@
+package com.goktech.olala.client.controller.order;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author sanming
+ * @Classname OrderCartController
+ * @Description TODO
+ * @Date 2020/11/28 10:22
+ */
+@RestController
+public class OrderCartController {
+
+}
